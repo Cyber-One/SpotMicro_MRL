591de6cba03da3841f5b849e31159b55544fa83d
Cyber-One <ray.edgley@gmail.com>
2021-12-03 19:33:11 +1100

    Create Gestures.py

---
+++ Spot/Gestures.py
@@ -0,0 +1,26 @@
+#################################################################
+#                                                               #
+# Program Code for Spot Micro MRL                               #
+# Of the Cyber_One YouTube Channel                              #
+# https://www.youtube.com/cyber_one                             #
+#                                                               #
+# This is version 0.1                                           #
+# Divided up into sub programs                                  #
+# Coded for the Nixie Version of MyRobotLab.                    #
+#                                                               #
+# Running on MyRobotLab (MRL) http://myrobotlab.org/            #
+# Spot Micro MRL is a set of Python scripts that run inside     #
+# the MRL system                                                #
+#                                                               #
+# Gestures.py                                                   #
+# This file is a group of commands that perform actions         #
+#                                                               #
+#################################################################
+print "Starting the various Gestures Services"
+
+def SpotSit():
+    FLShoulder.rest()
+    FRShoulder.rest()
+    BLShoulder.rest()
+    BRShoulder.rest()
+
