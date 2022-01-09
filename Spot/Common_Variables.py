e6e695c3066f8d89a0028fb8180eae2d695c650a
Cyber-One <ray.edgley@gmail.com>
2022-01-09 13:38:39 +1100

    Update Common_Variables.py

--- Spot/Common_Variables.py
+++ Spot/Common_Variables.py
@@ -52,5 +52,5 @@ LastRightPing = 0
 LWF = 135   # Length between the Wrist joint and the foot
 LTW = 110   # Length between the Arms joint and the Wrist joint
 LST = 50    # Length between the Shoulder joint and the center line of the Arm
-LYS = 95    # Length between the center Y plane and the shoulder joint
+LYS = 90    # Length between the center Y plane and the shoulder joint
 LXS = 38    # Length between the center X plane and the shoulder joint
