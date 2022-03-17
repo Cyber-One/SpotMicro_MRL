7054c6f3786cb9a92b80c61468fce15bf0092d6f
Cyber-One <ray.edgley@gmail.com>
2022-03-17 14:50:38 +1100

    Update InverseKinematics.py

--- Spot/Life/InverseKinematics.py
+++ Spot/Life/InverseKinematics.py
@@ -22,6 +22,7 @@ print "Starting the Inverse Kinematics Functions"
 
 def legInverseKinematics(LSFx, LAFy, Zt):
     Error = 0
+    print LSFx, ", LSFx:", LAFy, ", LAFy:", Zt, "Zt:"
     # all inputs x, y, and z are relative to the shoulder joint.
     # we will assume the offsets from the center of the bot have already been applied.
     # Lets first work out the length between the shoulder joint and the foot
@@ -32,87 +33,107 @@ def legInverseKinematics(LSFx, LAFy, Zt):
     LTFz = math.sqrt(LSF*LSF - LST*LST)
     Ai = math.asin(LSFx/LSF)
     Ao = math.acos(LST/LSF)
-    WorkServoS = math.degrees(Ai + Ao) - 90
-    if WorkServoS < ShoulderMin:
+    ServoS = math.degrees(Ai + Ao)
+    print ServoS, ", ServoS:", math.degrees(Ai), ", Ai:", math.degrees(Ao), "Ao:"
+    if ServoS < ShoulderMin:
         Error = 1
-        WorkServoS = ShoulderMin
-    if WorkServoS > ShoulderMax:
+        ServoS = ShoulderMin
+    if ServoS > ShoulderMax:
         Error = 2
-        WorkServoS = ShoulderMax
+        ServoS = ShoulderMax
     LTF = math.sqrt((LTFz*LTFz) + (LAFy*LAFy))
     # Working through a bug.
     if (LTW+LWF) < LTF:
         print "Warning, LTF is longer than LTW and LWF combined, this is impossible"
-        print LSFx, ", LSFx:", LAFy, ", LAFy:", Zt, "Zt:"
+        Error = 7
+    else:
         print LTFz, ", LTFz:", LTF, ", LTF:", ((LTW*LTW) + (LWF*LWF) - (LTF*LTF))/(2*LTW*LWF), "C:"
-    ServoWR = math.acos(((LTW*LTW) + (LWF*LWF) - (LTF*LTF))/(2*LTW*LWF))
-    WorkServoW = math.degrees(ServoWR)
-    if WorkServoW < WristMin:
-        Error = 5
-        WorkServoW = WristMin
-    if WorkServoW > WristMax:
-        Error = 6
-        WorkServoW = WristMax
-    Afw = math.asin((math.sin(math.radians(WorkServoW))*LWF)/LTF)
-    Af = math.acos(LTFz/LTF)
-    WorkServoA = math.degrees(Afw+Af) - 90
-    if WorkServoA < ArmMin:
-        Error = 3
-        WorkServoA = ArmMin
-    if WorkServoA > ArmMax:
-        Error = 4
-        WorkServoA = ArmMax
-    return {"Error":Error, "Shoulder":WorkServoS, "Arm":WorkServoA, "Wrist":WorkServoW}
+        ServoWR = math.acos(((LTW*LTW) + (LWF*LWF) - (LTF*LTF))/(2*LTW*LWF))
+        ServoW = math.degrees(ServoWR)
+        print ServoW, "ServoW:"
+        if ServoW < WristMin:
+            Error = 5
+            ServoW = WristMin
+        if ServoW > WristMax:
+            Error = 6
+            ServoW = WristMax
+        Afw = math.asin((math.sin(math.radians(ServoW))*LWF)/LTF)
+        Af = math.acos(LTFz/LTF)
+        ServoA = math.degrees(Afw - Af) + 90
+        print ServoA, ", ServoA:", Afw, ", Afw:", Af, "Af:"
+        if ServoA < ArmMin:
+            Error = 3
+            ServoA = ArmMin
+        if ServoA > ArmMax:
+            Error = 4
+            ServoA = ArmMax
+    return {"Error":Error, "Shoulder":ServoS, "Arm":ServoA, "Wrist":ServoW}
 
 def setServosInverseKinematics(Leg, X, Y, Z, Speed):
     if Leg == 0: # Front Left
         #FL_Leg(0)
         ServoPos = legInverseKinematics(X - LXS, Y - LYS, Z)
-        FLShoulder.moveTo(ServoPos.get("Shoulder"))
-        FLArm.moveTo(ServoPos.get("Arm"))
-        FLWrist.moveTo(ServoPos.get("Wrist"))
+        # Error trapping, only move if no error occured
+        if ServoPos.get("Error") = 0:
+            FLShoulder.moveTo(ServoPos.get("Shoulder"))
+            FLArm.moveTo(ServoPos.get("Arm"))
+            FLWrist.moveTo(ServoPos.get("Wrist"))
     if Leg == 1: # Front Right
         #FR_Leg(0)
         ServoPos = legInverseKinematics(-(X+LXS), Y - LYS, Z)
-        FRShoulder.moveTo(ServoPos.get("Shoulder"))
-        FRArm.moveTo(ServoPos.get("Arm"))
-        FRWrist.moveTo(ServoPos.get("Wrist"))
+        # Error trapping, only move if no error occured
+        if ServoPos.get("Error") = 0:
+            FRShoulder.moveTo(ServoPos.get("Shoulder"))
+            FRArm.moveTo(ServoPos.get("Arm"))
+            FRWrist.moveTo(ServoPos.get("Wrist"))
     if Leg == 2: # Back Left
         #BL_Leg(0)
         ServoPos = legInverseKinematics(X - LXS, Y + LYS, Z)
-        BLShoulder.moveTo(ServoPos.get("Shoulder"))
-        BLArm.moveTo(ServoPos.get("Arm"))
-        BLWrist.moveTo(ServoPos.get("Wrist"))
+        # Error trapping, only move if no error occured
+        if ServoPos.get("Error") = 0:
+            BLShoulder.moveTo(ServoPos.get("Shoulder"))
+            BLArm.moveTo(ServoPos.get("Arm"))
+            BLWrist.moveTo(ServoPos.get("Wrist"))
     if Leg == 3: # Back Right
         #BR_Leg(0)
         ServoPos = legInverseKinematics(-(X+LXS), Y + LYS, Z)
-        BRShoulder.moveTo(ServoPos.get("Shoulder"))
-        BRArm.moveTo(ServoPos.get("Arm"))
-        BRWrist.moveTo(ServoPos.get("Wrist"))
+        # Error trapping, only move if no error occured
+        if ServoPos.get("Error") = 0:
+            BRShoulder.moveTo(ServoPos.get("Shoulder"))
+            BRArm.moveTo(ServoPos.get("Arm"))
+            BRWrist.moveTo(ServoPos.get("Wrist"))
         
 def moveFoot(Leg, X, Y, Z, Speed):
     if Leg == 0: # Front Left
         FL_Leg(0)
         ServoPos = legInverseKinematics(FL_X+LXS + X, FL_Y-LYS + Y, FL_Z + Z)
-        FLShoulder.moveTo(ServoPos.get("Shoulder"))
-        FLArm.moveTo(ServoPos.get("Arm"))
-        FLWrist.moveTo(ServoPos.get("Wrist"))
+        # Error trapping, only move if no error occured
+        if ServoPos.get("Error") = 0:
+            FLShoulder.moveTo(ServoPos.get("Shoulder"))
+            FLArm.moveTo(ServoPos.get("Arm"))
+            FLWrist.moveTo(ServoPos.get("Wrist"))
     if Leg == 1: # Front Right
         FR_Leg(0)
-        ServoPos = legInverseKinematics(FL_X-LXS + X, FL_Y-LYS + Y, FL_Z + Z)
-        FRShoulder.moveTo(ServoPos.get("Shoulder"))
-        FRArm.moveTo(ServoPos.get("Arm"))
-        FRWrist.moveTo(ServoPos.get("Wrist"))
+        ServoPos = legInverseKinematics((FR_X-LXS) + X, (FR_Y-LYS) + Y, FR_Z + Z)
+        # Error trapping, only move if no error occured
+        if ServoPos.get("Error") = 0:
+            FRShoulder.moveTo(ServoPos.get("Shoulder"))
+            FRArm.moveTo(ServoPos.get("Arm"))
+            FRWrist.moveTo(ServoPos.get("Wrist"))
     if Leg == 2: # Back Left
         BL_Leg(0)
-        ServoPos = legInverseKinematics(FL_X+LXS + X, FL_Y+LYS + Y, FL_Z + Z)
-        BLShoulder.moveTo(ServoPos.get("Shoulder"))
-        BLArm.moveTo(ServoPos.get("Arm"))
-        BLWrist.moveTo(ServoPos.get("Wrist"))
+        ServoPos = legInverseKinematics(BL_X+LXS + X, BL_Y+LYS + Y, BL_Z + Z)
+        # Error trapping, only move if no error occured
+        if ServoPos.get("Error") = 0:
+            BLShoulder.moveTo(ServoPos.get("Shoulder"))
+            BLArm.moveTo(ServoPos.get("Arm"))
+            BLWrist.moveTo(ServoPos.get("Wrist"))
     if Leg == 3: # Back Right
         BR_Leg(0)
-        ServoPos = legInverseKinematics(FL_X-LXS + X, FL_Y+LYS + Y, FL_Z + Z)
-        BRShoulder.moveTo(ServoPos.get("Shoulder"))
-        BRArm.moveTo(ServoPos.get("Arm"))
-        BRWrist.moveTo(ServoPos.get("Wrist"))
+        ServoPos = legInverseKinematics(BR_X-LXS + X, BR_Y+LYS + Y, BR_Z + Z)
+        # Error trapping, only move if no error occured
+        if ServoPos.get("Error") = 0:
+            BRShoulder.moveTo(ServoPos.get("Shoulder"))
+            BRArm.moveTo(ServoPos.get("Arm"))
+            BRWrist.moveTo(ServoPos.get("Wrist"))
         
