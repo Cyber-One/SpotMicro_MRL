b0c9c7b0d882042f43ad6ac8f6ad16c79977e154
Cyber-One <ray.edgley@gmail.com>
2022-04-03 17:34:21 +1000

    Update Gestures.py

--- Spot/Gestures.py
+++ Spot/Gestures.py
@@ -55,4 +55,45 @@ def rest(Speed):
     BLWrist.rest()
     BRWrist.rest()
 
-
+#################################################################
+# layDown(Speed)                                                #
+# When laying down, we want the dog to be as low to the ground  #
+# as we can make it.                                            #
+# This means the Wrist will be set to the minimum, that is bent #
+# as far as we can get it and the Arm will be bent back at a    #
+# matching angle within a triangle so that the Wrist part of    #
+# the leg is parrallel to the body. An equalateral triangle.    #
+# Since the Arm is straight down at 90 degrees, we can add the  #
+# Wrist Min angle to 90 to get the target Arm servo position.   #
+#################################################################
+def Laydown(Speed):
+    print "Laying Down"
+    # First of the sanity checks.
+    if Speed > 1.0 or Speed < 0.01:
+        Speed = 1.0
+    # Set all the base speeds
+    FLShoulder.setSpeed(FLShoulderVelocity * Speed)
+    FRShoulder.setSpeed(FRShoulderVelocity * Speed)
+    BLShoulder.setSpeed(BLShoulderVelocity * Speed)
+    BRShoulder.setSpeed(BRShoulderVelocity * Speed)
+    FLArm.setSpeed(FLArmVelocity * Speed)
+    FRArm.setSpeed(FRArmVelocity * Speed)
+    BLArm.setSpeed(BLArmVelocity * Speed)
+    BRArm.setSpeed(BRArmVelocity * Speed)
+    FLWrist.setSpeed(FLWristVelocity * Speed)
+    FRWrist.setSpeed(FRWristVelocity * Speed)
+    BLWrist.setSpeed(BLWristVelocity * Speed)
+    BRWrist.setSpeed(BRWristVelocity * Speed)
+    # Now send the legs to the rest position
+    FLShoulder.rest()
+    FRShoulder.rest()
+    BLShoulder.rest()
+    BRShoulder.rest()
+    FLArm.moveTo(WristMin + 90)
+    FRArm.moveTo(WristMin + 90)
+    BLArm.moveTo(WristMin + 90)
+    BRArm.moveTo(WristMin + 90)
+    FLWrist.moveTo(WristMin)
+    FRWrist.moveTo(WristMin)
+    BLWrist.moveTo(WristMin)
+    BRWrist.moveTo(WristMin)
