8926ca3bb31dabab2905ed57f17f08184e80a824
Cyber-One <ray.edgley@gmail.com>
2022-04-09 17:52:58 +1000

    Update Gestures.py

--- Spot/Gestures.py
+++ Spot/Gestures.py
@@ -50,6 +50,7 @@ def rest(Speed):
 # the leg is parrallel to the body. An equalateral triangle.    #
 # Since the Arm is straight down at 90 degrees, we can add the  #
 # Wrist Min angle to 90 to get the target Arm servo position.   #
+# Note: WristMin = 50.0                                         #
 #################################################################
 def Laydown(Speed):
     print "Laying Down"
@@ -80,9 +81,23 @@ def Up(Speed):
     # To help prevent that, I rotate all the arms back 
     # as far they will go, moving the center of mass
     # forward of the center of contact.
-    FLArm.moveTo(ArmMax)
-    FRArm.moveTo(ArmMax)
-    BLArm.moveTo(ArmMax)
-    BRArm.moveToBlocking(ArmMax)
+    FLArm.moveTo(ArmMax-1)
+    FRArm.moveTo(ArmMax-1)
+    BLArm.moveTo(ArmMax-1)
+    BRArm.moveToBlocking(ArmMax-1)
+    setAllServoSpeeds(Speed)
     # Now send the legs to the rest position
-    sMoveFeetTo(-93.0, 87.405, -207.645, 93.0, 87.405, -207.645, -93.0, -92.595, -207.645, 93.0, -92.595, -207.645, Speed)
+    #sMoveFeetTo(-93.0, 87.405, -207.645, 93.0, 87.405, -207.645, -93.0, -92.595, -207.645, 93.0, -92.595, -207.645, Speed)
+    lMoveFeetTo("L", -93.0, 87.405, -207.645, "L", 93.0, 87.405, -207.645, "L", -93.0, -92.595, -207.645, "L", 93.0, -92.595, -207.645, Speed, 5)
+
+def Crouch(Speed):
+    sMoveFeetTo(-93.0, 87.405, -150.0, 93.0, 87.405, -150.0, -93.0, -92.595, -150.0, 93.0, -92.595, -150.0, 0.3)
+
+def Sit(Speed):
+    sMoveFeetTo(-93.0, 10.0, -219.0, 93.0, 10.0, -219.0, -93.0, -105.677, -113.0, 93.0, -105.677, -113.0, 0.3)
+    
+def LeanForward(Speed):
+    setAllServoSpeeds(Speed)
+    lMoveFeetTo("L", -93.0, 37.405, -207.645, "L", 93.0, 37.405, -207.645, "L", -93.0, -142.595, -207.645, "L", 93.0, -142.595, -207.645, Speed, 5)
+    sleep(3.0)
+    lMoveFeetTo("L", -93.0, 87.405, -207.645, "L", 93.0, 87.405, -207.645, "L", -93.0, -92.595, -207.645, "L", 93.0, -92.595, -207.645, Speed, 5)
