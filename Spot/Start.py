3ae865a5c1f4a53cff7ceade663a24ff4af0e120
Cyber-One <ray.edgley@gmail.com>
2023-01-26 14:10:18 +1100

    Update Start.py

--- Spot/Start.py
+++ Spot/Start.py
@@ -4,7 +4,7 @@
 # Of the Cyber_One YouTube Channel                              #
 # https://www.youtube.com/cyber_one                             #
 #                                                               #
-# This is version 0.1                                           #
+# This is version 0.2                                           #
 # Divided up into sub programs                                  #
 # Coded for the Nixie Version of MyRobotLab.                    #
 #                                                               #
@@ -12,10 +12,6 @@
 # Spot Micro MRL is a set of Python scripts that run inside     #
 # the MRL system                                                #
 #                                                               #
-# StartConfig.py                                                #
-# This file is called by the Python Service when it starts up   #
-# once a config has been created.  This then starts all the     #
-# other required scripts.                                       #
 #                                                               #
 #################################################################
 # Because you might want to place your robots files into a      #
@@ -38,6 +34,17 @@ RobotsName = "Spot"
 #execfile(RuningFolder+'/Common_Variables.py')
 
 #################################################################
+# This next line is to correct a PWM Frequency error in my 	#
+# PCA9685. This error may not effect you.			#
+# The only true way to know is to measure the PWM signal	#
+# frequency being sent from the PCA9685 to the servos.		#
+# You should have 60Hz, if not, you will need to adjust the PWM	#
+# frequency up or down to get 60Hz				#
+# Use small changes or you could damage your servos.		#
+#################################################################
+Back.setPWMFreq(1, 54)
+
+#################################################################
 # Setup the Foot Class routines.                                #
 # Once the class has been run, we can create an instance by     #
 # calling the Feet method.  This requires all the servo objects #
@@ -51,6 +58,12 @@ legs.disableAutoLevel()
 
 #################################################################
 # Setup the MPU6050 calibration and callback functions          #
+# the setXGyroOffset() sets the calibration for the Gyro in the #
+# MPU6050, to know what value to set we get that from the       #
+# MPU6050 with the getGyroXSelfTestFactoryTrim()                #
+# The updateOrientation() function is the call back target for  #
+# the MPU6050 service, we then use this to call the balance     #
+# routines in the FootClass.
 #################################################################
 if runtime.isStarted("MPU6050A"):
     MPU6050A.setXGyroOffset(MPU6050A.getGyroXSelfTestFactoryTrim())
