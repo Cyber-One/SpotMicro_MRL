1369c81f6e4b3e6ebbdb48595ea944da108b75af
Cyber-One <ray.edgley@gmail.com>
2023-02-05 08:24:23 +1100

    Update FootClass.py

--- Spot/FootClass.py
+++ Spot/FootClass.py
@@ -713,7 +713,7 @@ class Feet():
             self.FR.setServoPos(self.FR.shoulder.pos+(Shoulder/Steps), self.FR.arm.pos+(Arm/Steps), self.FR.wrist.pos+(Wrist/Steps))
             self.BL.setServoPos(self.BL.shoulder.pos+(Shoulder/Steps), self.BL.arm.pos+(Arm/Steps), self.BL.wrist.pos+(Wrist/Steps))
             self.BR.setServoPos(self.BR.shoulder.pos+(Shoulder/Steps), self.BR.arm.pos+(Arm/Steps), self.BR.wrist.pos+(Wrist/Steps))
-            slep(10)
+            sleep(10)
         self.imuUpdateFK()
 
         #self.FL.moveServos(Shoulder, Arm, Wrist, Steps)
