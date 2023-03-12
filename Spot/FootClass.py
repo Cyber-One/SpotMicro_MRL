21dd1c8a5299172c11cdb70ba46b0d429a08ec1e
Cyber-One <ray.edgley@gmail.com>
2023-03-12 18:41:34 +1100

    Update FootClass.py

--- Spot/FootClass.py
+++ Spot/FootClass.py
@@ -802,7 +802,9 @@ class Feet():
 
     def levelRobot(self):
         pos = self.getRobotICoMXYZ()
-        self.moveRobotRPoR4D(0, 0, (math.sin(self.roll)*self.FL.LXS)-(math.sin(self.pitch)*self.FL.LYS), 0, 0, (-math.sin(self.roll)*self.FR.LXS)-(math.sin(self.pitch)*self.FR.LYS), 0, 0, (math.sin(self.roll)*self.BL.LXS)+(math.sin(self.pitch)*self.BL.LYS), 0, 0, (-math.sin(self.roll)*self.BR.LXS)+(math.sin(self.pitch)*self.BR.LYS))
+        #self.moveRobotRPoR4D(0, 0, (math.sin(self.roll)*self.FL.LXS)-(math.sin(self.pitch)*self.FL.LYS), 0, 0, (-math.sin(self.roll)*self.FR.LXS)-(math.sin(self.pitch)*self.FR.LYS), 0, 0, (math.sin(self.roll)*self.BL.LXS)+(math.sin(self.pitch)*self.BL.LYS), 0, 0, (-math.sin(self.roll)*self.BR.LXS)+(math.sin(self.pitch)*self.BR.LYS))
+        self.moveRobotRPoR4D(0, 0, (math.sin(self.roll)*self.FL.LXS), 0, 0, (-math.sin(self.roll)*self.FR.LXS), 0, 0, (math.sin(self.roll)*self.BL.LXS), 0, 0, (-math.sin(self.roll)*self.BR.LXS))
+        #self.moveRobotRPoR4D(0, 0, (-(math.sin(self.pitch)*self.FL.LYS), 0, 0, -(math.sin(self.pitch)*self.FR.LYS), 0, 0, (math.sin(self.pitch)*self.BL.LYS), 0, 0, (math.sin(self.pitch)*self.BR.LYS))
         
     def calculateBalancePoint(self):
         # Lets work out the FL, BR line, lets call this LR.
