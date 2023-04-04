0a76260baaf098024d497d470f292fbc156302d6
Cyber-One <ray.edgley@gmail.com>
2023-04-05 09:32:51 +1000

    Update Gestures.py

--- Spot/Gestures.py
+++ Spot/Gestures.py
@@ -131,3 +131,17 @@ def sit(Steps = 10, Time = 0.05):
     gestureStatus = 2
     print(legs)
 
+def startWalk(FWD = 5, Lift = 50, Steps = 10, Time = 0.01):
+    # Front Left Leg move forward.
+    moveRobotICoMPoR4D(0, FWD*3, Lift, 0, -FWD, 0, 0, -FWD, 0, 0, -FWD, 0, Steps, Time)
+    moveRobotICoMPoR4D(0, FWD*3, -Lift, 0, -FWD, 0, 0, -FWD, 0, 0, -FWD, 0, Steps, Time)
+    # Front Right Leg move forward.
+    moveRobotICoMPoR4D(0, -FWD, 0, 0, FWD*3, Lift, 0, -FWD, 0, 0, -FWD, 0, Steps, Time)
+    moveRobotICoMPoR4D(0, -FWD, 0, 0, FWD*3, -Lift, 0, -FWD, 0, 0, -FWD, 0, Steps, Time)
+    # Back Left Leg move forward.
+    moveRobotICoMPoR4D(0, -FWD, 0, 0, -FWD, 0, 0, FWD*3, Lift, 0, -FWD, 0, Steps, Time)
+    moveRobotICoMPoR4D(0, -FWD, 0, 0, -FWD, 0, 0, FWD*3, -Lift, 0, -FWD, 0, Steps, Time)
+    # Back Right Leg move forward.
+    moveRobotICoMPoR4D(0, -FWD, 0, 0, -FWD, 0, 0, -FWD, 0, 0, FWD*3, Lift, Steps, Time)
+    moveRobotICoMPoR4D(0, -FWD, 0, 0, -FWD, 0, 0, -FWD, 0, 0, FWD*3, -Lift, Steps, Time)
+    #moveRobotICoMPoR4D(FLX, FLY, FLZ, FRX, FRY, FRZ, BLX, BLY, BLZ, BRX, BRY, BRZ, Steps, Time)
