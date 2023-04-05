697923fe90ed388069f1cfadde21fa191940292a
Cyber-One <ray.edgley@gmail.com>
2023-04-05 15:22:29 +1000

    Create Stop.py

---
+++ Spot/Stop.py
@@ -0,0 +1,21 @@
+#################################################################
+#                                                               #
+# Program Code for Spot Micro MRL                               #
+# Of the Cyber_One YouTube Channel                              #
+# https://www.youtube.com/cyber_one                             #
+#                                                               #
+# This is version 0.2                                           #
+# Divided up into sub programs                                  #
+# Coded for the Nixie Version of MyRobotLab.                    #
+#                                                               #
+# Running on MyRobotLab (MRL) http://myrobotlab.org/            #
+# Spot Micro MRL is a set of Python scripts that run inside     #
+# the MRL system                                                #
+#                                                               #
+#                                                               #
+#################################################################
+
+# Shutdown script.
+# We need to make sure the robot is in the rest position before 
+# the robot shuts down to prevent it from falling over.
+rest(10, 0.03)
